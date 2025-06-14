057adb69611a039b26c512674af70ab18aef0bb3
dsh1345 <dsh1345@naver.com>
2025-06-14 16:59:18 +0900

    백준 1874 실버2

---
+++ bj1874.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        Stack<Integer> stack = new Stack<>(); 
+        StringBuilder sb = new StringBuilder();
+        boolean check = true;
+        int j = 1;
+        for(int i=0;i<n;i++) {
+        	int num = sc.nextInt();
+        	
+    		while(j <= num) {
+    			stack.push(j++);
+    			sb.append("+\n");
+    		}	
+			if(stack.peek() == num) {
+				stack.pop();
+				sb.append("-\n");
+			}
+			else {
+				check = false;
+				break;
+			}		
+        }
+        if (check) {
+        	System.out.print(sb.toString());
+        } else {
+        	System.out.println("NO");
+        }
+    }
+}
