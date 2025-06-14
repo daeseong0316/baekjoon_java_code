747db11be68dce5b2512926f76dbabf8468783f2
dsh1345 <dsh1345@naver.com>
2025-06-14 19:08:18 +0900

    백준 1874 실버2

--- bj1874.java
+++ bj1874.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class bj1874 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
