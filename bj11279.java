d92541d38b4faa145d9e3aa9cfd7be0da694209d
dsh1345 <dsh1345@naver.com>
2025-06-29 22:26:00 +0900

    백준 11279 실버2

--- bj11279.java
+++ bj11279.java
@@ -9,20 +9,20 @@ public class bj11279 {
         
         int n = Integer.parseInt(br.readLine());	// 연산 개수 n개
         
-        Queue<Integer> minheap = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0;i<n;i++) {
         	int num = Integer.parseInt(br.readLine());	// 정수 num
         	
         	if(num == 0) {	// num이 0일 때
-        		if(minheap.isEmpty()) {	// que가 비워져 있으면
+        		if(maxheap.isEmpty()) {	// que가 비워져 있으면
         			bw.write("0\n");	// 0 출력
         		}
         		else {      
-        			bw.write(minheap.poll() + "\n");	// 비워져 있지 않으면 가장 작은 값 출력(root 노드 출력) 
+        			bw.write(maxheap.poll() + "\n");	// 비워져 있지 않으면 가장 큰 값 출력(root 노드 출력) 
         		}
         	}
         	else {	
-        		minheap.offer(num);	// 0이 아니면 que에 num 값 넣기
+        		maxheap.offer(num);	// 0이 아니면 que에 num 값 넣기
         	}
         }
         
