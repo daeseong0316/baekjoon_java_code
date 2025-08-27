4cf3b11a80ea13309edda822ccb00c877413dbf6
dsh1345 <dsh1345@naver.com>
2025-08-27 14:27:57 +0900

    dasdioas

--- bjasdf.java
+++ bjasdf.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-public class bjresr {
+public class bjasdf {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        bw.write("rwrfewf\n");
+        bw.write("dsafjsai");
         
         bw.close();
     }
