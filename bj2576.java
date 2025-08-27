a715dc76b078282e9fb18bf9f716c69748b207a2
dsh1345 <dsh1345@naver.com>
2025-08-27 16:03:10 +0900

    백준 2576 브론즈4

---
+++ bj2576.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class bj2576 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        int count = 7;
+        for(int i=0;i<count;i++) {
+        	int n = Integer.parseInt(br.readLine());
+        	if((n % 2) == 1) {
+        		list.add(n);
+        	}
+        }
+
+        Collections.sort(list);
+        
+        int sum = 0;
+        for(int i=0;i<list.size();i++) {
+        	sum += list.get(i);
+        }
+        
+        if(list.size() < 1) {
+        	bw.write("-1");
+        }
+        else {
+        	bw.write(sum + "\n" + list.get(0));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
