0b512414f6a1dff40570c2aede58814db5bf150d
dsh1345 <dsh1345@naver.com>
2025-08-27 14:38:16 +0900

    safsa

---
+++ silver1/bjzxcv.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class bjzxcv {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("dsafjsai");
+        
+        bw.close();
+    }
+}
