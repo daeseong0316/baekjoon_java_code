2510242ec59dae9eeec8fa0eb848009e374c9f31
dsh1345 <dsh1345@naver.com>
2025-08-27 14:09:38 +0900

    fsf

---
+++ silver1/bj_imsi.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class bj_imsi {    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("24324");
+    
+    }
+}
