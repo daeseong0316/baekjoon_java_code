98f3b02067238a5da536906a546d25b957aeed88
dsh1345 <dsh1345@naver.com>
2025-09-05 09:05:07 +0900

    돌 게임 2_백준 9656 실버5

---
+++ silver5/bj9656.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class bj9656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = Integer.parseInt(br.readLine());	// 돌 n개
+        
+        boolean[] dp = new boolean[n+1];
+        
+        // dp 초기값
+        if(n >= 1) dp[1] = true;
+        if(n >= 2) dp[2] = false;
+        if(n >= 3) dp[3] = true;
+        
+        // 점화식
+        for(int i=4;i<=n;i++) {
+        	dp[i] = !dp[i-1] || !dp[i-3];
+        }
+        
+        // 결과
+        if(dp[n]) {
+        	bw.write("CY");
+        }
+        else {
+        	bw.write("SK");
+        }
+        
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
